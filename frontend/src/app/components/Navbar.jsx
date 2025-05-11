b50ab9f2291128ec88f52408f0b05bb8c66f2d41
manshu777 <79352371+Manshu777@users.noreply.github.com>
2025-05-11 12:11:05 +0530

    add-newones

--- frontend/src/app/components/Navbar.jsx
+++ frontend/src/app/components/Navbar.jsx
@@ -10,39 +10,39 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [loginType, setLoginType] = useState('');
-const [otpsended,SetotpSend]=useState(false)
-const [otp,setotp]=useState('')
-const [loading,setLoading]=useState(false)
+  const [otpsended, SetotpSend] = useState(false)
+  const [otp, setotp] = useState('')
+  const [loading, setLoading] = useState(false)
 
-const [email,setemail]=useState("")
-const navigation=useRouter()
-const handelotp=async()=>{
-  setLoading(true)
-if(email.trim()){
-  const response=await axios.post(`${baseurl}/send-otp`,{email:email})
-  console.log(response.data)
-  SetotpSend(true)
-}
+  const [email, setemail] = useState("")
+  const navigation = useRouter()
+  const handelotp = async () => {
+    setLoading(true)
+    if (email.trim()) {
+      const response = await axios.post(`${baseurl}/send-otp`, { email: email })
+      console.log(response.data)
+      SetotpSend(true)
+    }
 
-setLoading(false)
-}
-
-const handelSendopt=async()=>{
-  setLoading(true)
-  if(otp.length==6){ 
-
-    const response=await axios.post(`${baseurl}/verify-otp`,{email:email,otp:otp})
-
-
-  if(response.data.success){
-    localStorage.setItem("port_tok",response.data.token)
-    setShowModal(false)
-    navigation.push('/candidate/candidate-login')
+    setLoading(false)
   }
-  }
-  setLoading(false)
 
-}
+  const handelSendopt = async () => {
+    setLoading(true)
+    if (otp.length == 6) {
+
+      const response = await axios.post(`${baseurl}/verify-otp`, { email: email, otp: otp })
+
+
+      if (response.data.success) {
+        localStorage.setItem("port_tok", response.data.token)
+        setShowModal(false)
+        navigation.push('/candidate/candidate-login')
+      }
+    }
+    setLoading(false)
+
+  }
 
 
   return (
@@ -120,72 +120,72 @@ const handelSendopt=async()=>{
       {showModal && (
         <div className="fixed inset-0 bg-[#00000054] bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full relative">
-            <h2 className="text-xl mb-4 font-bold">{loginType} Enter your Email 
+            <h2 className="text-xl mb-4 font-bold">{loginType} Enter your Email
             </h2>
             <input
               type="email"
               value={email}
               placeholder="Enter your email"
               className="w-full border p-2 rounded"
-              onChange={(e)=>setemail(e.target.value)}
+              onChange={(e) => setemail(e.target.value)}
               disabled={otpsended}
-            /> 
+            />
 
 
-{ otpsended &&
-  <div>
-  <label htmlFor='otp'>Otp</label>
-  <input
-              type="number"
-              value={otp}
-              placeholder="Enter your Otp"
-              className="w-full border p-2 rounded"
-              onChange={(e)=>setotp(e.target.value)}
-              maxLength={6}
-              
-            /> 
-  
-  </div>
-}
+            {otpsended &&
+              <div>
+                <label htmlFor='otp'>Otp</label>
+                <input
+                  type="number"
+                  value={otp}
+                  placeholder="Enter your Otp"
+                  className="w-full border p-2 rounded"
+                  onChange={(e) => setotp(e.target.value)}
+                  maxLength={6}
+
+                />
+
+              </div>
+            }
 
 
 
 
 
             <p className='text-slate-400 my-4'>
-            By continuing, you agree  service and Privacy Policy
+              By continuing, you agree  service and Privacy Policy
             </p>
             {otpsended &&
-            <button
-            className="bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700"
-            onClick={handelSendopt}
-            disabled={loading}
-          >
-           {loading?"Loading...":"verifyotp"}  
-          </button>
-
-            }
-   
-   { !otpsended &&<>
-            <div className="flex justify-end space-x-2">
-              <button
-                onClick={() => setShowModal(false)}
-                className="bg-gray-300 px-3 py-1 rounded hover:bg-gray-400"
-              >
-                Cancel
-              </button>
               <button
                 className="bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700"
-                onClick={handelotp}
+                onClick={handelSendopt}
                 disabled={loading}
               >
-                
-                {loading?"Loading...":"Submit"} 
+                {loading ? "Loading..." : "verifyotp"}
               </button>
-             
-            </div>
+
+            }
+
+            {!otpsended && <>
+              <div className="flex justify-end space-x-2">
+                <button
+                  onClick={() => setShowModal(false)}
+                  className="bg-gray-300 px-3 py-1 rounded hover:bg-gray-400"
+                >
+                  Cancel
+                </button>
+                <button
+                  className="bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700"
+                  onClick={handelotp}
+                  disabled={loading}
+                >
+
+                  {loading ? "Loading..." : "Submit"}
+                </button>
+
+              </div>
             </>
-   }
+            }
             <button
               onClick={() => setShowModal(false)}
               className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
